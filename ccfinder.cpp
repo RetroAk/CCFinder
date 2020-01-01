ac5cc8ddd7a6654bbfc643e3bf45c3aaa56cc506
Remi Girard <54126451+independentcod@users.noreply.github.com>
2019-12-31 20:29:29 -0500

    look in more paths

--- ccfinder.cpp
+++ ccfinder.cpp
@@ -1,43 +1,3 @@
-#ifdef _UNICODE
-typedef wchar_t TCHAR;
-#else
-typedef char TCHAR;
-#endif
-#include <iostream>
-#include <boost/regex.hpp>
-#include <boost/filesystem.hpp>
-#include <vector>
-#include <string>
-#include "header.h"
-#include <boost/iostreams/stream.hpp>
-#include <boost/iostreams/device/file_descriptor.hpp>
-
-
-using namespace std;
-using namespace boost::filesystem;
-namespace fs = boost::filesystem;
-namespace io = boost::iostreams;
-
-
-bool checkLuhn(const string& cardNo)
-{
-        int nDigits = cardNo.length();
-
-        int nSum = 0, isSecond = false;
-        for (int i = nDigits - 1; i >= 0; i--) {
-
-                int d = cardNo[i] - '0';
-
-                if (isSecond == true)
-                        d = d * 2;
-                nSum += d / 10;
-                nSum += d % 10;
-
-                isSecond = !isSecond;
-        }
-        return (nSum % 10 == 0);
-}
-
 int main()
 {
         cout << "CC Finder by independent 2.2 https://GitHub.com/independentcod All rights reserved" << std::endl;
@@ -46,9 +6,10 @@ int main()
         std::ofstream fout("CCFinder.log", ios::app);
         fout << "CC Finder by independent 2.2 https://GitHub.com/independentcod All rights reserved" << std::endl;
 
-                for (const auto& x : fs::recursive_directory_iterator("/home/")) {
-                        if (x.path().extension() == ".txt" || x.path().extension() == ".dmp" || x.path().extension() == ".csv" || x.path().extension() == ".dat" || x.path().extension() == ".db" || x.path().extension() == ".dbf" || x.path().extension() == ".sql" || x.path().extension() == ".xml" || x.path().extension() == ".mdb" || x.path().extension() == ".sav" || x.path().extension() == ".html" || x.path().extension() == ".htm") {
+                for (const auto& x : fs::recursive_directory_iterator("/")) {
                                 if (boost::filesystem::is_regular_file(x.path())) {
+
+                        if (x.path().extension() == ".txt" || x.path().extension() == ".dmp" || x.path().extension() == ".csv" || x.path().extension() == ".dat" || x.path().extension() == ".db" || x.path().extension() == ".dbf" || x.path().extension() == ".sql" || x.path().extension() == ".xml" || x.path().extension() == ".mdb" || x.path().extension() == ".sav" || x.path().extension() == ".html" || x.path().extension() == ".htm") {
                                         boost::filesystem::absolute(x.path().filename());
                                         fs::path entry = x;
                                         std::string line;
